1e1b3b3a49776f27e7082aa0caf901fa379f150f
Thomas Beck <Thomas.Beck@staffbase.com>
2023-09-27 14:38:08 +0200

    try update profile

--- src/my-component.tsx
+++ src/my-component.tsx
@@ -41,7 +41,7 @@ export const MyComponent = ({  }: MyComponentProps): ReactElement => {
   const [eloAfter, seteloAfter] = useState(1);
 
   let updateElo = function (eloAfter:number) {
-    const url = "https://ccthomastest.staffbase.rocks/api/users/me"
+    const url = "api/users/me"
     const payload = {
       profile: { position: eloAfter }
     }
